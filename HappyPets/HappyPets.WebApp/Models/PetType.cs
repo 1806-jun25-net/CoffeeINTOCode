f28651209c69e4085b97d87b5f7bd0c2d1a76c27
Yessebell Rios <yessebell.rios@upr.edu>
2018-07-28 21:08:24 -0400

    auth changes

---
+++ HappyPets/HappyPets.WebApp/Models/PetType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HappyPets.Data
+{
+    public partial class PetType
+    {
+        public PetType()
+        {
+            Pet = new HashSet<Pet>();
+        }
+
+        public int TypesId { get; set; }
+        public string TypesName { get; set; }
+
+        public ICollection<Pet> Pet { get; set; }
+    }
+}
