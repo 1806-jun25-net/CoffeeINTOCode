5c6f729659d4df85435286ae8a4b9ca34cf0f459
Jean Carlo Semprit Rodriguez <jc.semprit@hotmail.com>
2018-08-01 01:40:35 -0400

    Add User repository operations

--- HappyPets.API/HappyPets.Library/Repository/UserRepository.cs
+++ HappyPets.API/HappyPets.Library/Repository/UserRepository.cs
@@ -1,16 +1,86 @@
 ﻿using HappyPets.Data;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using HappyPets.Library.Repository.CRUDs;
 
 namespace HappyPets.Library.Repository
 {
 
-    public partial class Repository 
+    public partial class Repository
     {
+        // Users
+        public Users GetUser(int id)
+        {
+            var user = _repo.GetUsersByID(id);
+            return user;
+        }
+
+        public IEnumerable<Users> GetUsers(string value, int option = 0)
+        {
+            IEnumerable<Users> user;
+
+            switch(option)
+            {
+                case 1:
+                    user = _repo.GetUserByEmail(value);
+                    break;
+
+                case 2:
+                    user = _repo.GetUserByLastName(value);
+                    break;
+
+                default:
+                    user = _repo.GetUserByName(value);
+                    break;
+            }
+
+            return user;
+        }
+
+        public IEnumerable<Users> GetUsers(int locationId)
+        {
+            var user = _repo.GetUserByLocation(locationId);
+            return user;
+        }
+
+        public IEnumerable<Users> GetAllUsers()
+        {
+            var user = _repo.GetAllUser();
+            return user;
+        }
+
+        public void EditUser(Users users)
+        {
+            _repo.UpdateUser(users);
+        }
+
+        public void CreateNewUser(Users user)
+        {
+            _repo.CreateUser(user);
+        }
+
+        public void CreateNewUser(
+            string firstName,
+            string lastName,
+            string email,
+            string password,
+            string streetAddress,
+            int locationId)
+        {
+            _repo.CreateUser(
+                firstName,
+                lastName,
+                email,
+                password,
+                streetAddress,
+                locationId
+            );
+        }
 
 
+
+
+
+
+        // Employee
     }
 }
 
