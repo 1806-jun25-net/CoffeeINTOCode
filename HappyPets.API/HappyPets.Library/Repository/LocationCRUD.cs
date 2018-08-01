c19df0658ff35b7e66b9c698000858d89fa6fdc9
Jean Carlo Semprit Rodriguez <jc.semprit@hotmail.com>
2018-07-31 23:41:45 -0400

    Create CRUD operation for Location model

--- HappyPets.API/HappyPets.Library/Repository/LocationCRUD.cs
+++ HappyPets.API/HappyPets.Library/Repository/LocationCRUD.cs
@@ -1,10 +1,72 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HappyPets.Data;
+
 namespace HappyPets.Library.Repository
 {
-    public class LocationCRUD
+    public partial class Repository
     {
-        public LocationCRUD()
+        // CRUD operation for Location
+        // Create
+        public void CreateLocation(Location location)
         {
+            _db.Add(location);
+
+        }
+
+        public void CreateLocation(string locationName)
+        {
+            var location = new Location
+            {
+                LocationName = locationName
+            };
+
+            _db.Add(location);
+
+        }
+
+        // Read Single
+        public Location GetLocationByID(int id)
+        {
+            var location = _db.Location.Find(id);
+            string errMsg = "Not Location found with that ID";
+
+            if (location == null)
+            {
+                throw new ArgumentException(errMsg, nameof(id));
+            }
+
+            return location;
+
+        }
+
+        // Read All
+        public IEnumerable<Location> GetAllLocation()
+        {
+            var allLocation = _db.Location.ToList();
+            return allLocation;
+        }
+
+        // Update
+        public void UpdateLocation(Location location)
+        {
+            _db.Location.Update(location);
+        }
+
+
+        // Delete
+        public void DeleteLotion(int Id)
+        {
+            var location = _db.Location.Find(Id);
+            string errMsg = "Not Location found with that ID";
+
+            if (location == null)
+            {
+                throw new ArgumentException(errMsg, nameof(Id));
+            }
+
+            _db.Remove(location);
         }
     }
 }
