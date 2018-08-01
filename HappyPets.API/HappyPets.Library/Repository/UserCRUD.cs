ee5785ba514d43d78f9baadc9e1182aa5c80a067
Jean Carlo Semprit Rodriguez <jc.semprit@hotmail.com>
2018-07-31 22:46:10 -0400

    Add CRUD operation on the Repository for Users Model

--- HappyPets.API/HappyPets.Library/Repository/UserCRUD.cs
+++ HappyPets.API/HappyPets.Library/Repository/UserCRUD.cs
@@ -1,10 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HappyPets.Data;
+
 namespace HappyPets.Library.Repository
 {
-    public class UserCRUD
+    public partial class Repository
     {
-        public UserCRUD()
+        // CRUD operation for users_id
+        // Create
+        public void CreateUser(Users user)
         {
+            _db.Add(user);
+            
+        }
+
+        public void CreateUser(
+            string firstName, 
+            string lastName, 
+            string email,
+            string password, 
+            string streetAddress, 
+            int locationId)
+        {
+            var user = new Users
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Email = email,
+                Passwords = password,
+                StreetAddress = streetAddress,
+                LocationId = locationId
+            };
+
+            _db.Add(user);
+            
+        }
+
+        // Read Single
+        public Users GetUsersByID(int id)
+        {
+            var user = _db.Users.Find(id);
+            return user;
+
+        }
+
+        // Read All
+        public IEnumerable<Users> GetAllUser()
+        {
+            var alluser = _db.Users.ToList();
+            return alluser;
+        }
+
+        // Update
+        public void UpdateUser(Users user) 
+        {
+            _db.Users.Update(user);
+        }
+
+
+        // Delete
+        public void DeleteUser(int Id)
+        {
+            var userToDelete = _db.Users.Find(Id);
+            string errMsg = "Not User found with thad ID";
+
+            if(userToDelete == null)
+            {
+                throw new ArgumentException(errMsg, nameof(Id));
+            }
+
+            _db.Remove(userToDelete);
         }
     }
 }
