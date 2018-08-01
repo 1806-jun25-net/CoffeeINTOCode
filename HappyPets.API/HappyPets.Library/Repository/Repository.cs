309e4e920faf4a1258783db50a8bd93c9f29666e
Jean Carlo Semprit Rodriguez <jc.semprit@hotmail.com>
2018-08-01 01:03:43 -0400

    Add Repository class and implement constructor

--- HappyPets.API/HappyPets.Library/Repository/Repository.cs
+++ HappyPets.API/HappyPets.Library/Repository/Repository.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using HappyPets.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using HappyPets.Library.Repository.CRUDs;
+
 namespace HappyPets.Library.Repository
 {
-    public class Repository
+
+    public partial class Repository
     {
-        public Repository()
+        private readonly RepositoryCRUDs _repo;
+
+        public Repository(RepositoryCRUDs repo)
         {
+            _repo = repo ?? throw new ArgumentException(nameof(repo));
+
         }
     }
 }
+
+
