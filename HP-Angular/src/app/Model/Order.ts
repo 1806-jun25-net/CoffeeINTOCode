0a347217268674acb0c4e96321bc4932ccfe7686
yessebell23 <yessebell.rios@upr.edu>
2018-08-06 02:09:45 -0400

    angular page

--- HP-Angular/src/app/Model/Order.ts
+++ HP-Angular/src/app/Model/Order.ts
@@ -1,5 +1,22 @@
 export class Order {
 user_id: number
+locationId: number
+orderTime: string
+totalCost: number
+employee: number
+
+
+/*
+ "orderId": 1,
+        "locationId": 1,
+        "employeeId": 1,
+        "orderTime": "2018-08-01T23:18:00",
+        "totalCost": 20,
+        "employee": null,
+        "location": null,
+        "appointments": [],
+        "cart": []
+*/
 
 
 }
